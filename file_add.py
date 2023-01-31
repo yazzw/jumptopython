57ba160687f8d967ec9f0f5d3d05bb8287cd3ad3
yazzw <yazzw@naver.com>
2023-01-31 22:16:14 +0900

    change it to count 31

--- file_add.py
+++ file_add.py
@@ -1,6 +1,6 @@
 # Add last of the data
 f = open("mobis_1630435.txt",'a')
-for i in range(11,21) :
+for i in range(11,31) :
     f.write("%d \n"%i)
 f.write("This is end of this file.\n")
 f.write('='*50)
